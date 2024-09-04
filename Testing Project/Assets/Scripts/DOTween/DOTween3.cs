86288c117f1012ac9ae2583e4ec4b2ee4483c487
Taehyeon88 <163229667+Taehyeon88@users.noreply.github.com>
2024-09-04 19:21:43 +0900

    Update DOTween3.cs
    1학기 때의 코드 가져오기

--- Testing Project/Assets/Scripts/DOTween/DOTween3.cs
+++ Testing Project/Assets/Scripts/DOTween/DOTween3.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class DOTween3 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float duration = 1f;
+    public Image image;
     void Start()
     {
-        
+        image = GetComponent<Image>();
+
+        image.DOFade(0f, duration)
+                .SetEase(Ease.InOutQuad)
+                .SetAutoKill(false)
+                .Pause()
+                .OnComplete(() => Debug.Log("UI ¿Ï·á"));
+
+        image.DOPlay();
     }
 
     // Update is called once per frame
