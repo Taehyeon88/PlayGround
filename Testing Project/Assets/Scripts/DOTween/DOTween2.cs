c282789b3c392c5aa010f91b18c0348d60306e3b
Taehyeon88 <163229667+Taehyeon88@users.noreply.github.com>
2024-09-04 19:19:44 +0900

    Update DOTween2.cs
    1학기 때의 코드 가져오기

--- Testing Project/Assets/Scripts/DOTween/DOTween2.cs
+++ Testing Project/Assets/Scripts/DOTween/DOTween2.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class DOTween2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool isPunch = false;
+    
+    private Renderer renderer;
     void Start()
     {
-        
+        renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Shift))
+        {
+            if(!isPunch)
+            {
+                isPunch = true;
+                transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0.5f), 0.1f, 10, 1).OnComplete(EndPuch);
+            }
+        }
         
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Color color = new Color(Random.value, Random.value, Random.value);
+
+            renderer .material.DOColor(color, 1f)
+                .SetEase(Ease.InOutQuad)
+                .SetAutoKill(false)
+                .Pause()
+                .OnComplete(() => Debug.Log("Colorº¯È¯ ¿Ï·á"));
+                renderer.material.DOPlay();
+    }
+    void EndPuch()
+    {
+        isPunch = false;
     }
 }
